f5cbfb8924a727fa2c03397647db8ecee10a903e
Mark Nguyen <mgb.nguyen@gmail.com>
2023-02-19 20:45:58 +0000

    Arctic scene blender file; glb; loading babylonjs

--- src/babylonjs/BabylonScene.js
+++ src/babylonjs/BabylonScene.js
@@ -1,9 +1,15 @@
 import * as BABYLON from "@babylonjs/core";
+import "@babylonjs/loaders";
+
+import arcticModel from "./models/Arctic.glb";
 
 export default class BabylonScene {
     constructor(canvas) {
         const engine = new BABYLON.Engine(canvas);
         const scene = new BABYLON.Scene(engine);
+
+        BABYLON.SceneLoader.AppendAsync(arcticModel, "");
+
         const box = BABYLON.MeshBuilder.CreateBox("box", {});
         scene.createDefaultCameraOrLight(true, true, true);
         scene.createDefaultEnvironment();
