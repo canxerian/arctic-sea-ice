708d77cdf628373d179deb2816705cb42e0fc906
Mark Nguyen <mgb.nguyen@gmail.com>
2023-02-19 21:04:35 +0000

    Camera

--- src/babylonjs/BabylonScene.js
+++ src/babylonjs/BabylonScene.js
@@ -11,11 +11,27 @@ export default class BabylonScene {
         BABYLON.SceneLoader.AppendAsync(arcticModel, "");
 
         const box = BABYLON.MeshBuilder.CreateBox("box", {});
-        scene.createDefaultCameraOrLight(true, true, true);
-        scene.createDefaultEnvironment();
+        box.position = new BABYLON.Vector3(0, 10, 0);
+        const camera = this.createCamera();
 
         engine.runRenderLoop(() => {
             scene.render();
         });
+    }
+
+    createCamera(canvas) {
+        const Deg2Rad = Math.PI / 180;
+
+        const cam = new BABYLON.ArcRotateCamera(
+            "Main Camera",
+            0,
+            70 * Deg2Rad,
+            20,
+            new BABYLON.Vector3(0, 0, 0)
+        );
+        cam.upperBetaLimit = 90 * Deg2Rad;
+
+        cam.attachControl(canvas, true, true);
+        return cam;
     }
 }
