7c9725f7c4475190d5fa5144c168adb18bb34191
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2023-03-26 22:04:24 +0100

    Replace sections with details, so that can be collapsed/expanded

--- src/components/HelpUI.jsx
+++ src/components/HelpUI.jsx
@@ -18,28 +18,35 @@ const HelpUI = () => {
             <dialog open={isOpen} id="help-ui-dialog">
                 <main className="custom-scrollbar">
                     <h1>Arctic Sea Ice</h1>
-                    <section>
-                        <h2>What can I do with this app?</h2>
+
+                    <details>
+                        <summary>
+                            <h2>What can I do with this app?</h2>
+                        </summary>
                         <dl>
                             <dt>Filter data:</dt>
                             <dd>Click on the buttons to filter. The list of data updates accordingly</dd>
 
-                            <dt>View historical ice data</dt>
+                            <dt>View historical ice data:</dt>
                             <dd>Swipe or scroll the list. The 3D visual updates accordingly.</dd>
 
-                            <dt>View satellite imagery</dt>
+                            <dt>View satellite imagery:</dt>
                             <dd>Put the scene in "2D" mode</dd>
                         </dl>
-                    </section>
+                    </details>
 
-                    <section>
-                        <h2>Why was this project created?</h2>
+                    <details>
+                        <summary>
+                            <h2>Why was this project created?</h2>
+                        </summary>
                         <p><a href="https://nsidc.org/home">The National Snow and Ice Data Center</a> measure data about our world using very nice satellites and generously make them publicly available. However, specialist software is sometimes required to process and visualise it</p>
                         <p>By making the data interactive, intuitive and visual, the data becomes more accessible and engaging. The web app can run on desktop, tablet and mobile.</p>
-                    </section>
+                    </details>
 
-                    <section>
-                        <h2>What technologies were used to create this?</h2>
+                    <details>
+                        <summary>
+                            <h2>What technologies were used to create this?</h2>
+                        </summary>
                         <dl>
                             <dt>3D rendering:</dt>
                             <dd><a href="https://www.babylonjs.com/">babylon.js</a> and custom shaders</dd>
@@ -53,20 +60,25 @@ const HelpUI = () => {
                             <dt>3D modelling:</dt>
                             <dd><a href="https://blender.org/">Blender</a></dd>
                         </dl>
-                    </section>
+                    </details>
 
-                    <section>
-                        <h2>Can we discuss working with you?</h2>
+                    <details>
+                        <summary>
+                            <h2>Can we discuss working with you?</h2>
+                        </summary>
                         <p>Sure, hello@coding-canxerian.com</p>
-                    </section>
+                    </details>
 
-                    <section>
-                        <h2>Who are you?</h2>
+                    <details>
+                        <summary>
+                            <h2>Who are you?</h2>
+                        </summary>
                         <p>Mark Nguyen, a Cancerian who like to make things with art, code and design</p>
-                        <div>
-
-                        </div>
-                    </section>
+                        <ul>
+                            <li>Instagram: @coding.canxerian</li>
+                            <li>Twitter @coding.canxerian</li>
+                        </ul>
+                    </details>
                 </main>
                 <button title="Close dialog modal" onClick={() => setIsOpen(false)}>Close</button>
             </dialog>
