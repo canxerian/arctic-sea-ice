92f694818d926a4e7457bf15caee6de6111e149d
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2023-03-30 23:05:41 +0100

    Additional interest to the stylised sky gradient

--- src/babylonjs/stylisedSky/StylisedSky.fragment.glsl
+++ src/babylonjs/stylisedSky/StylisedSky.fragment.glsl
@@ -12,14 +12,16 @@ void main(void) {
     vec3 sunPos = vec3(500, 4, 400);
     vec3 sunDir = normalize(_CamPos - sunPos);
     vec3 viewDir = normalize(vViewDir);
+    vec3 sunCol = vec3(0.36, 0.13, 0.26);
 
-    float sun = dot(viewDir, sunDir);
+    float sun = dot(viewDir, sunDir);       // not really a "sun" anymore.. just a splash of colour in the direction of the sun
 
-    sun = step(0.999, sun);
+    sun = smoothstep(0.56, 1.0, sun) * 0.09;
+    sunCol *= sun;
 
     vec3 gradient = mix(_Colour1, _Colour2, vUV.y);
 
-    vec3 col = gradient + vec3(sun);
+    vec3 col = gradient + sunCol;
 
     gl_FragColor = vec4(col, 1.0);
 }
