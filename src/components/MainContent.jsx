927237445e0e2031811291ba65b9d6f95598edac
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2023-04-14 15:42:11 +0100

    Typo

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -27,7 +27,7 @@ const MainContent = forwardRef(({ isLoaded }, ref) => {
                 </div>
             </section>
             <section className={`title ${loadedClassName}`}>
-                <h3>Arctice Sea Ice Data Explorer</h3>
+                <h3>Arctic Sea Ice Data Explorer</h3>
                 <h1>An interactive, 3D explorer of the Arctic Sea Index</h1>
                 <p>Data sourced from <a href="https://nsidc.org/data/seaice_index/data-and-image-archive">NSIDC data and image archive</a></p>
             </section>
